7ce4d33251945961e9e2aa3e66399729a7725cd6
refracta <refracta@koreatech.ac.kr>
2023-11-05 19:04:06 +0900

    Minor fix

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -13,17 +13,9 @@ $dockerConfig = @{
 $myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent();
 $myWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($myWindowsID);
 $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator;
-if ($myWindowsPrincipal.IsInRole($adminRole))
+if (-not $myWindowsPrincipal.IsInRole($adminRole))
 {
-    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)";
-    $Host.UI.RawUI.BackgroundColor = "DarkBlue";
-	Clear-Host;
-}
-else {
-    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
-    $newProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
-    $newProcess.Verb = "runas";
-    [System.Diagnostics.Process]::Start($newProcess);
+    Write-Host "Administrator privileges are required to execute this script"
     Exit;
 }
 

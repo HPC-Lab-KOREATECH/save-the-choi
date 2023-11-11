6624880cb7f01d8e835ffc27aaba41287c6245f9
refracta <refracta@koreatech.ac.kr>
2023-11-11 17:33:50 +0900

    Minor fix

--- scripts/linux/stc/run.sh
+++ scripts/linux/stc/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
-tmux kill-session -t stc 2>/dev/null
-tmux new -d -s stc
-tmux send-keys -t stc "bash" C-m
-tmux send-keys -t stc "/opt/stc/core.sh" C-m
+if [ -n "$DISPLAY" ]; then
+    tmux kill-session -t stc 2>/dev/null
+    tmux new -d -s stc
+    tmux send-keys -t stc "bash" C-m
+    tmux send-keys -t stc "/opt/stc/core.sh" C-m
+else
+    echo "[stc] This is not a graphical session. Must be run in a graphical session."
+fi
